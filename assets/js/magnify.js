aae959c427ec8073cc63569c9d240285622224f2
Ace <akhilark1088@gmail.com>
2020-11-23 00:45:24 -0500

    Adding music with clickable overlay.

--- assets/js/magnify.js
+++ assets/js/magnify.js
@@ -1,17 +1,17 @@
-const cardContent = document.getElementById('content');
-cardContent.addEventListener('click', magnify);
+// const cardContent = document.getElementById('content');
+// cardContent.addEventListener('click', magnify);
 
-const closeButton = document.getElementById('close');
-closeButton.addEventListener('click', close);
+// const closeButton = document.getElementById('close');
+// closeButton.addEventListener('click', close);
 
-function magnify() {
-    console.log('clicked cardContent');
-    cardContent.addClass('magnified');
-    closeButton.style.display = 'block';
-}
+// function magnify() {
+//     console.log('clicked cardContent');
+//     cardContent.addClass('magnified');
+//     closeButton.style.display = 'block';
+// }
 
-function close() {
-    console.log('clicked close');
-    cardContent.removeClass('magnified');
-    closeButton.style.display = 'none';
-}
+// function close() {
+//     console.log('clicked close');
+//     cardContent.removeClass('magnified');
+//     closeButton.style.display = 'none';
+// }
